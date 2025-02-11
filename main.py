7668e79b9e6700d6781f837efe2ece6d5aefc2bc
TimurchikOgyrchik <134474047+TimurchikOgyrchik@users.noreply.github.com>
2025-02-11 22:13:53 +0900

    3 zadanie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask import url_for
+
 i = 0
 app = Flask(__name__)
 
@@ -8,11 +9,28 @@ app = Flask(__name__)
 def index():
     return "И на Марсе будут яблони цвести!"
 
+
 @app.route('/promotion')
 def promotion():
     g = ['Человечество вырастает из детства.', "Человечеству мала одна планета.",
-                     'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '</br>'.join(g)
+
+
+@app.route('/image_mars')
+def image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}"><img>
+                  </body>
+                </html>"""
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
