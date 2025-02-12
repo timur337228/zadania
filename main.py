9e444a7b8d997b9281bcbaadece27561f3fe3942
TimurchikOgyrchik <134474047+TimurchikOgyrchik@users.noreply.github.com>
2025-02-12 16:09:02 +0900

    4 zadanie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import random
+
 from flask import Flask, request
 from flask import url_for
+import re
 
 i = 0
 app = Flask(__name__)
@@ -28,6 +31,32 @@ def image():
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/mars.jpg')}"><img>
+                  </body>
+                </html>"""
+
+
+@app.route('/<path:path>/promotion_image', defaults={'path': ''})
+def promotion_image(path):
+    g = ['Человечество вырастает из детства.', "Человечеству мала одна планета.",
+         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    h = []
+    bootstrap = ['secondary', 'success', 'danger', 'warning']
+    for i in g:
+        print(i)
+        h.append(f'<div class="alert alert-{random.choice(bootstrap)}"role="alert">{i}')
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    </br><img src="{url_for('static', filename='img/mars.jpg')}"><img><br>
+                    {'</div>'.join(h)}
+                    </div>
                   </body>
                 </html>"""
 
